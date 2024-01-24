acf11e2451cdaea9eea3a4df39d1b179ac33515e
Álvaro Cañas <acaygo@gmail.com>
2024-01-24 18:10:27 +0100

    examen POO

---
+++ examenes/POO.py
@@ -0,0 +1,17 @@
+from typeguard import typechecked
+@typechecked
+class stack:
+
+
+
+    def __init__(self):
+
+
+    def is_empy(self):
+
+
+    def push(self, int):
+
+
+    def pop(self, int):
+
