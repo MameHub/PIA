af4e73f00115f320447dea11f6515daf935adddf
Álvaro Cañas <acaygo@gmail.com>
2024-03-06 20:06:19 +0100

    prueba ej2

---
+++ API-REST/FBI.py
@@ -0,0 +1,23 @@
+"""
+author: Álvaro Cañas
+date: 2024-03-06
+
+El FBI tiene un recorte de personal informático y solicitan nuestra ayuda, quieren saber cuantos fugitivos
+tienen registrados en cada una de sus oficinas, para ello han habilitado una API a la que puedes acceder
+desde aqui: https://www.fbi.gov/wanted/api
+
+El progrema debe mostrar el nombre de cada oficina (ordenado) y la cantidad de fugitivos registrados.
+También debe mostrar la cantidad de fugitivos no registrados en ninguna oficina.
+
+Ten en cuenta que cada consulta muestra un número limitado de registros, vas a tener que hacer consultas
+iterativas enviando como parámetro la página de la consulta hasta que ya no queden páginas que consultar.
+"""
+
+import requests as rq
+import json as js
+
+
+response = rq.get('https://api.fbi.gov/wanted/v1/list')
+data = js.loads(response.content)
+print(data['total'])
+print(data['items'][0]['title'])
